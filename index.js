55302d234c75a369654ba574355432eeb72147c1
VPO92 <vpo92@users.noreply.github.com>
2022-09-27 16:45:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,12 +9,18 @@ console.log(jsonData);
 
 app.get('/', (req, res) => {
   res.send("Bienvenue sur l'API Penelope GB");
-})
+});
 
 app.get('/files', (req, res) => {
   res.send(jsonData);
-})
+});
+
+app.get('/files/:id', (req, res) => {
+  console.log("download file :"+req.params.id);
+  const file = `${__dirname}/dummy.xml`;
+  res.download(file);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
