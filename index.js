e844f80f32201a709f174f11f237243db6517dac
VPO92 <vpo92@users.noreply.github.com>
2022-09-27 16:10:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+const jsonData = require('./data.json');
+
+console.log(jsonData);
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send("Bienvenue sur l'API Penelope GB");
+})
+
+app.get('/files', (req, res) => {
+  res.send(jsonData);
 })
 
 app.listen(port, () => {
